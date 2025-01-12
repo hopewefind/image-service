9e8bb06ad0ef65afcbc17c3235d6b0ddb994efe6
Mateusz Bartosz <mateusz.bartosz@gmail.com>
2025-01-12 22:03:46 +0100

    Initial commit

---
+++ src/main/kotlin/com/hopewefind/sun/SunApplication.kt
@@ -0,0 +1,11 @@
+package com.hopewefind.sun
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SunApplication
+
+fun main(args: Array<String>) {
+    runApplication<SunApplication>(*args)
+}
